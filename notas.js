b82ba010231c41bb66448ae93fe4e614c616b75c
mariadrey17 <belydrey@yahoo.com.ar>
2022-12-27 20:02:37 -0300

    poniendo punto y coma para tener la costumbre de la buena practica

--- notas.js
+++ notas.js
@@ -4,7 +4,7 @@
 //luego ingresa las tres notas calculando la nota final
 //estos datos salen por alerta y por consola 
 // el ciclo anidado  en nota final la primera opcion es si es mayor igual que 21 esta aprobado, opcion 2 si es mayor igual a 12 esta aprobado , y la tercera opcion si es menor a 12 entonces desprobado tiene que recuperar
-//la consola va a mostrar nombre , cusro y notafinal del alumno
+//la consola va a mostrar nombre , curso y notafinal del alumno
 
 
 
@@ -16,7 +16,7 @@ for  (let  i=1  ; i <= 4 ; i++) {
     
     alert ("mi nombre  es "  + nombre  );
     alert ( "mi curso  es "  +  curso );
-    console .log ( nombre ,curso)
+    console .log ( nombre ,curso);
     
     
     
@@ -34,7 +34,7 @@ for  (let  i=1  ; i <= 4 ; i++) {
     
     
     let promedioFinal =sumarPromedio (nota1,nota2,nota3);
-    console .log (promedioFinal)
+    console .log (promedioFinal);
   
   
    promedioFinal= 21
@@ -43,12 +43,12 @@ for  (let  i=1  ; i <= 4 ; i++) {
   
   
    if (notaFinal >= 21){
-    alert ("Felicitaciones pasastes de año !!!Estás aprobado")
+    alert ("Felicitaciones pasastes de año !!!Estás aprobado");
   
    } else if ( notaFinal >=12)
    {alert ("Estás aprobado segui trabajando así")} 
    else {
-    alert ("Está desaprobado debes dar recuperatorio")
+    alert ("Está desaprobado debes dar recuperatorio");
    }
   
   
